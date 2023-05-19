6ea4811ea1b2f69d6f68beac26d41d2bfd7119cb
iibrahim70 <aviknuhash70@gmail.com>
2023-05-19 23:41:08 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+require('dotenv').config()
+const port = process.env.PORT || 5000; 
+
+// middleware 
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Car Is Running'); 
+})
+
+app.listen(port, () => {
+  console.log(`Car Craze Is Running On Port ${port}`);
+})
