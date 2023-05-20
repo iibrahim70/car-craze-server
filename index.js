360f5eefaf06b464dc89437ab9a3668ca74666cf
iibrahim70 <aviknuhash70@gmail.com>
2023-05-20 14:25:39 +0600

    create a post method for receiving data from the front end to backend

--- index.js
+++ index.js
@@ -23,7 +23,15 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
+    await client.connect();
+
+    
+
+    app.post('/toys', async (req, res) => {
+      const newToys = req.body; 
+      console.log(newToys);
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
