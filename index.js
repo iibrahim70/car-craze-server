30727c75ea861d26f8eea71d3518cb6b4fc69cc0
iibrahim70 <aviknuhash70@gmail.com>
2023-05-21 02:02:19 +0600

    create a delete api for delete my toys

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000; 
@@ -43,6 +43,14 @@ async function run() {
       const result = await toysCollection.find(query).toArray();
       res.send(result);
     });
+
+    // delete my toy 
+    app.delete('/mytoys/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}; 
+      const result = await toysCollection.deleteOne(query);
+      res.send(result);
+    })
 
     // add toy
     app.post('/toys', async (req, res) => {
