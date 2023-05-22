1c3ab0cbc253beb32f58a84168f31641b00d1984
iibrahim70 <aviknuhash70@gmail.com>
2023-05-22 21:52:44 +0600

    added sort api and search api

--- index.js
+++ index.js
@@ -29,18 +29,40 @@ async function run() {
 
     // get all toy
     app.get('/toys', async (req, res) => {
-      const limit = parseInt(req.query.limit) || 20; 
+      const limit = parseInt(req.query.limit) || 20;
       const result = await toysCollection.find().limit(limit).toArray();
       res.send(result);
     });
 
-    // get my toy
+    // get serach toys
+    app.get('/serach-toys', async (req, res) => {
+      const limit = parseInt(req.query.limit) || 20;
+      const name = req.query.name; // Get the toy name from the query parameters
+      let query = {};
+      if (name) {
+        // If toy name is provided, create a query to search by name
+        query = { toyName: { $regex: name, $options: 'i' } }; // Case-insensitive search using regex
+      }
+      const result = await toysCollection.find(query).limit(limit).toArray();
+      res.send(result);
+    });
+
+    // get toy by using id
+    app.get('/toys/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}; 
+      const result = await toysCollection.findOne(query);
+      res.send(result);
+    })
+
+    // // get my toy and sort 
     app.get('/mytoys', async (req, res) => {
       let query = {};
       if (req.query?.email) {
         query = { sellerEmail: req.query.email };
       }
-      const result = await toysCollection.find(query).toArray();
+      const sort = req.query.sort || 'asc'; // Default sort order is ascending
+      const result = await toysCollection.find(query).sort({ price: sort === 'desc' ? -1 : 1 }).toArray();
       res.send(result);
     });
 
@@ -71,7 +93,7 @@ async function run() {
       const updatedToy = req.body;
       const toy = {
         $set: {
-          price: updatedToy.name, 
+          price: updatedToy.price, 
           quantity: updatedToy.quantity,
           description: updatedToy.description,
         }
