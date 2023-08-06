3bda2a6b179b3d6b898d13da6f478ef43a38de77
iibrahim70 <aviknuhash70@gmail.com>
2023-08-06 16:07:01 +0600

    update the mongo user pass and delete one api

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.anttmlo.mongodb.net/?retryWrites=true&w=majority`;
+const uri = process.env.mongodbUri
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -25,68 +25,70 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
 
-    const toysCollection = client.db('toysDB').collection('toys');
+    const carsCollection = client.db('carsDB').collection('cars');
+    const reviewsCollection = client.db('carsDB').collection('reviews');
+    const newCarsCollection = client.db('carsDB').collection('newArrivals');
 
-    // get all toy
-    app.get('/toys', async (req, res) => {
+    // get all the cars
+    app.get('/cars', async (req, res) => {
       const limit = parseInt(req.query.limit) || 20;
-      const result = await toysCollection.find().limit(limit).toArray();
+      const result = await carsCollection.find().limit(limit).toArray();
       res.send(result);
     });
 
-    // get serach toys
-    app.get('/serach-toys', async (req, res) => {
+    // get all the the reviews
+    app.get('/reviews', async (req, res) => {
+      const limit = parseInt(req.query.limit) || 12;
+      const result = await reviewsCollection.find().limit(limit).toArray();
+      res.send(result);
+    });
+
+    // get all the new collections 
+    app.get('/new-cars', async (req, res) => {
+      const result = await newCarsCollection.find().toArray(); 
+      res.send(result);
+    })  
+
+    // get cars by search 
+    app.get('/serach-cars', async (req, res) => {
       const limit = parseInt(req.query.limit) || 20;
-      const name = req.query.name; // Get the toy name from the query parameters
+      const name = req.query.name; 
       let query = {};
       if (name) {
-        // If toy name is provided, create a query to search by name
-        query = { toyName: { $regex: name, $options: 'i' } }; // Case-insensitive search using regex
+        query = { carsName: { $regex: name, $options: 'i' } };
       }
-      const result = await toysCollection.find(query).limit(limit).toArray();
+      const result = await carsCollection.find(query).limit(limit).toArray();
       res.send(result);
     });
 
-    // get toy by using id
-    app.get('/toys/:id', async (req, res) => {
+    // get car by using their id
+    app.get('/cars/:id', async (req, res) => {
       const id = req.params.id;
       const query = {_id: new ObjectId(id)}; 
-      const result = await toysCollection.findOne(query);
+      const result = await carsCollection.findOne(query);
       res.send(result);
     })
 
-    // // get my toy and sort 
-    app.get('/mytoys', async (req, res) => {
+    // // get my cars and sort 
+    app.get('/mycars', async (req, res) => {
       let query = {};
       if (req.query?.email) {
         query = { sellerEmail: req.query.email };
       }
       const sort = req.query.sort || 'asc'; // Default sort order is ascending
-      const result = await toysCollection.find(query).sort({ price: sort === 'desc' ? -1 : 1 }).toArray();
+      const result = await carsCollection.find(query).sort({ price: sort === 'desc' ? -1 : 1 }).toArray();
       res.send(result);
     });
 
-    // get toys by category
-    app.get('/shop-category/:text', async (req, res) => {
-      if (req.params.text == "sports-car" || req.params.text == "mini-fire-truck" || req.params.text == "police-car"){
-        console.log(req.params.text);
-        const result = await toysCollection.find({subCategory: req.params.text}).toArray();
-        console.log(result);
-        return res.send(result);
-      } 
-      const result = await toysCollection.find({}).toArray();
-      res.send(result);
-    });
-
-    // add toy
-    app.post('/toys', async (req, res) => {
-      const newToys = req.body; 
-      const result = await toysCollection.insertOne(newToys);
+    // add cars to the database
+    app.post('/cars', async (req, res) => {
+      const newCars = req.body; 
+      const result = await carsCollection.insertOne(newCars);
       res.send(result);
     })
     
     // update toy
-    app.put('/mytoys/:id', async (req, res) => {
+    app.put('/mycars/:id', async (req, res) => {
       const id = req.params.id;
       const filter = {_id: new ObjectId(id)}; 
       const options = {upsert: true};
@@ -98,15 +100,15 @@ async function run() {
           description: updatedToy.description,
         }
       }
-      const result = await toysCollection.updateOne(filter, toy, options);
+      const result = await carsCollection.updateOne(filter, toy, options);
       res.send(result); 
     })
 
     // delete my toy 
-    app.delete('/mytoys/:id', async (req, res) => {
+    app.delete('/mycars/:id', async (req, res) => {
       const id = req.params.id;
       const query = {_id: new ObjectId(id)}; 
-      const result = await toysCollection.deleteOne(query);
+      const result = await carsCollection.deleteOne(query);
       res.send(result);
     })
 
@@ -121,7 +123,7 @@ async function run() {
 run().catch(console.dir);
 
 app.get('/', (req, res) => {
-  res.send('Car Is Running'); 
+  res.send('Car Craze Is Running'); 
 })
 
 app.listen(port, () => {
